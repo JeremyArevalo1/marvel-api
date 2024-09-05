66395c837a2403b62572ca9bb7b73633cd3db0c3
Jeremy <jarevalo-2023528@kinal.edu.gt>
2024-09-04 23:04:55 -0600

    marvel a medias

---
+++ src/components/MarvelApp.jsx
@@ -0,0 +1,32 @@
+import CryptoJS from "crypto-js"
+import { useEffect, useState } from "react"
+
+
+export const MarvelApp = () => {
+  const [characters, setCharacter] = useState()
+
+  const time_stamp = new Date().getTime()
+  const private_api_key = '9a36a0e05164b37fc8c89f3ff45936bd71ff6376'
+  const public_api_key= '2e737a65cc7cae2fd0300b354eca14d3'
+
+  const hash = CryptoJS.MD5(time_stamp + private_api_key + public_api_key).toString()
+  
+  useEffect(() => {
+    const reqCharacter = async () => {
+      const resp = await fetch (`https://gateway.marvel.com:443/v1/public/characters?ts=${time_stamp}&apikey=${public_api_key}&hash=${hash}`)
+      const {data} = await resp.json()
+      data.results.map((characters) =>{
+        console.log(characters.name)
+      })
+    }
+    reqCharacter()
+    
+  
+    
+  }, [])
+  
+  
+  return (
+    <div>MarvelApp</div>
+  )
+}
