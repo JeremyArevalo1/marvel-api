3c5cf3319c5034b181b8ac00bbbbc2e1fc1bbef2
Informatica <informatica@alumno.local>
2024-09-05 17:12:08 -0600

    paginacion incompleta

--- src/components/MarvelApp.jsx
+++ src/components/MarvelApp.jsx
@@ -1,15 +1,15 @@
 import Pagination from '@mui/material/Pagination';
 import { useCharacters } from "../hooks/useCharacters"
 import { GridCharacters } from "./GridCharacters"
+import { BuscarMarvel } from './BucarMarvel';
 
 
 export const MarvelApp = () => {
-  
-  
-  //console.log(characters)
+  const {handleMarvel} = useCharacters()
   const {characters} = useCharacters()
   return (
     <>
+          <BuscarMarvel handleMarvel={handleMarvel}/>
           <GridCharacters characters={characters}/>
           <Pagination count={10} onChange={(e, value) => console.log(value)}/>
 
--- src/hooks/useCharacters.jsx
+++ src/hooks/useCharacters.jsx
@@ -7,21 +7,26 @@ import { reqCharacter } from "../service/characters"
 export const useCharacters = () => {
     const [characters, setCharacter] = useState()
 
-  
-  
-  
-  useEffect(() => {
-    
-    reqCharacter().then((data) => {
-        setCharacter(data.results)
-    })
-    
-  
-    
-  }, [])
+    useEffect(() => {
+
+        reqCharacter().then((data) => {
+            setCharacter(data.results)
+        })
+    }, [])
+
+    const handleMarvel = async (categoria, e) =>{
+        e.preventDefault()
+        try {
+            const data = await reqCharacter(categoria)
+            setCharacter(data)
+        } catch (error) {
+            console.error('Error fetching data:', error)
+        }
+    }
 
 
-  return {
-    characters,
-  }
+
+    return {
+        characters, handleMarvel
+    }
 }
